8eff15353fab7d0253807386d9dc95cdc50f0020
WhackyTomD <deeterinktom@gmail.com>
2024-09-20 11:04:25 +0200

    Right file push
    I have refactored the code, and put it in the right place for launch

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,205 +1,152 @@
 <script>
-  import { onMount } from "svelte"; //import onmount
-  import { get } from "svelte/store";
+  import { onMount } from 'svelte'; //import onmount
+import { get } from 'svelte/store';
 
-  let person = null;
+  let person = null; 
 
   const getapi = async () => {
-    const res = await fetch("https://fdnd.directus.app/items/person/14"); //api fetch
-    const data = await res.json(); //data fetch
-    console.log(data); //console log voor data retrieval
-    person = data.data; //verklaar de data aan het persoon person
-  };
+      const res = await fetch("https://fdnd.directus.app/items/person/14"); //api fetch
+      const data = await res.json(); //data fetch
+      console.log(data); //console log voor data retrieval
+      person = data.data; //verklaar de data aan het persoon person
+  }
 
   // gebruik onmount om de functie uit te voeren
   onMount(() => {
-    getapi();
-  });
+      getapi();
+  }); 
+
 </script>
+
 
 <div class="wrapper">
   <section class="main-section">
-    {#if person}
+      {#if person} 
       <!-- Als person data geladen is, laat dit zien -->
-      <img src={person.avatar} width="200px" height="240px" />
-      <h1>{person.name} {person.surname}</h1>
+       <div class="lightsaber1"></div>
+      <h1>{person.name} {person.surname}</h1> 
       <p>{person.bio}</p>
       <ul class="socials">
-        <li><a href={person.github_handle}>Github</a></li>
-        <li><a href={person.website}>Website</a></li>
+          <li><a href="{person.github_handle}">Github</a></li>
+          <li><a href="{person.website}">Website</a></li>
       </ul>
-      <h2>Mijn projecten</h2>
-      <ul class="projects">
-        <li class="project-card">
-          <h3>Project 1</h3>
-          <h4>Investables</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            natus eveniet sapiente ipsam quidem vel! Eius voluptas reiciendis
-            illum asperiores .
-          </p>
-          <a> Klikken </a>
-        </li>
-        <li class="project-card">
-          <h3>Project 1</h3>
-          <h4>Investables</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            natus eveniet sapiente ipsam quidem vel! Eius voluptas reiciendis
-            illum asperiores .
-          </p>
-          <a> Klikken </a>
-        </li>
-        <li class="project-card">
-          <h3>Project 1</h3>
-          <h4>Investables</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            natus eveniet sapiente ipsam quidem vel! Eius voluptas reiciendis
-            illum asperiores .
-          </p>
-          <a> Klikken </a>
-        </li>
-      </ul>
-      <h2>My socials</h2>
-    {:else}
-      <!-- Als person data aan het laden is, laat dit zien -->
+      <div class="lightsaber2"></div>
+      {:else}
+          <!-- Als person data aan het laden is, laat dit zien -->
       <h1>Loading Data...</h1>
-    {/if}
+      {/if}
+  
   </section>
 </div>
 
 <style>
-  .wrapper {
-    background-color: black;
-    padding: 2rem;
-    & .main-section {
-      overflow: hidden;
-      height: 100%;
-      max-width: 800px;
-      margin: 0 auto;
-      & img {
-        object-fit: cover;
-        object-position: top;
-      }
-      & h1 {
-        color: antiquewhite;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 3.5em;
-        padding-top: 1.5rem;
-        padding-bottom: 1.5rem;
-        border-bottom: 4px dotted antiquewhite;
-        margin: 0.25rem 0rem;
-      }
-      & h2 {
-        color: antiquewhite;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 2.8em;
-        padding-top: 1.5rem;
-        padding-bottom: 1.5rem;
-        border-top: 4px dotted orangered;
-        border-bottom: 4px dotted antiquewhite;
-        margin: 0.25rem 0rem;
-      }
-      & p {
-        color: antiquewhite;
-        font-family: Arial, Helvetica, sans-serif;
-        margin-bottom: 1.5rem;
-        border-left: 4px dotted antiquewhite;
-        padding-left: 1rem;
-        font-size: 1.5em;
-      }
-      & ul {
-        list-style-type: none;
-        padding-inline-start: 1rem;
-        padding-bottom: 0.5rem;
-        border-left: 4px dotted orangered;
-      }
-      & .socials {
-        display: flex;
-        flex-direction: column;
-        gap: 1.5rem;
-        & a {
-          font-size: 1.5em;
-          font-family: Arial, Helvetica, sans-serif;
-          color: rgb(255, 69, 0);
-          text-decoration: underline;
-          text-underline-offset: 4px;
-          transition: ease-out 0.3s;
-          &:hover,
-          &:focus {
-            border-radius: 4px;
-            background-color: antiquewhite;
-            padding: 10px 50px;
-            transition: ease-in 0.2s;
-            text-underline-offset: 8px;
-            color: rgb(232, 63, 2);
+ .wrapper{
+  width: 100%;
+  height: 100vh;
+  background: url(foto.jpg);
+  object-fit: contain;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+      & .main-section{
+          background-color: rgba(0, 0, 0, 0.63);
+          border-radius: 1em;
+          padding: 4em;
+          width: 300px;
+          color: white;
+          transition: ease-out 0.4s;
+          transform: matrix3d(0.9958652, -0.0027093, 0, -0.13e-05, -0.019669, 0.704708, 0, -0.0008721, 0, 0, 1, 0, 0, 35, 0, 1);
+          & h1 , p , a{
+              color: goldenrod;
+              font-family:  Helvetica ;
+              font-weight: bold;
+              word-spacing: 10px;
+              text-align: center;
           }
-        }
-      }
-      & .projects {
-        max-width: 100%;
-        display: flex;
-        gap: 2em;
-        overflow-x: auto;
-        scroll-snap-type: proximity;
-        scroll-snap-align: center;
-        padding-left: 2.5em;
-        & h3 {
-          color: antiquewhite;
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 2em;
-          padding-top: 1.5rem;
-          padding-bottom: 1.5rem;
-          margin: 0.25rem 0rem;
-        }
-        & h4 {
-          color: orangered;
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 1.3em;
-          margin: 0px;
-        }
-        & .project-card {
-          min-width: 450px; /* Ensure each project card has a minimum width */
-          display: flex;
-          flex-direction: column;
-          transition: ease-out 0.2s;
-          padding: 0em 2em;
-          & a {
-            display: none;
+          & .socials{
+              list-style-type: none;
+              display: flex;
+              justify-content: center;
+              flex-direction: column;
+              gap: 3em;
+              align-items: center;
+              padding-inline-start: 0px;
           }
-          &:hover {
-            cursor: pointer;
-            transition: ease-in 0.2s;
-            background-color: antiquewhite;
-            padding-bottom: 2em;
-            & h3 {
-              color: orangered;
-            }
-            & p {
-              color: black;
-              border-left: 4px dotted black;
-            }
-            & a {
-              display: block;
-              font-size: 1.5em;
-              font-family: Arial, Helvetica, sans-serif;
-              color: rgb(255, 69, 0);
-              text-decoration: underline;
-              text-underline-offset: 4px;
-              transition: ease-out 0.3s;
-              &:hover,
-              &:focus {
-                border-radius: 4px;
-                background-color: antiquewhite;
-                transition: ease-in 0.2s;
-                text-underline-offset: 8px;
-                color: rgb(232, 63, 2);
+          & a{
+              position: absolute;
+              z-index: 99;
+              margin-left: -30px;
+              text-align: center;
+              text-underline-offset: 2px;
+              transition: ease-out 0.2s;
+              &:hover{
+                  text-underline-offset: 8px;
+                  transition: ease-in 0.3s;
               }
-            }
           }
-        }
+          & .lightsaber1{
+              box-shadow: 40px 0px 20px  black;
+              transition: ease-in 0.4s;
+              position: absolute;
+              margin-top: -20px;
+              content: "";
+              height: 10px;
+              width: 48px;
+              border-radius: 2px;
+              background-color: red;
+                  &::before{
+                      border-radius: 2px;
+                  position: fixed;
+                  content: "";
+                  height: 12px;
+                  margin-top: -1px;
+                  margin-left: -2px;
+                  width: 50px;
+                  background-color: rgb(63, 63, 63);
+                  box-shadow: inset 0px 0px 5px rgb(19, 19, 19);
+                  }
+          }
+          & .lightsaber2{
+              box-shadow: 40px 0px 20px  black;
+              border-radius: 2px;
+              transition: ease-in 0.4s;
+              position: absolute;
+              margin-top: 20px;
+              content: "";
+              height: 10px;
+              width: 48px;
+              background-color: blue;
+              border-radius: 2px;
+                  &::before{
+                  position: fixed;
+                  border-radius: 2px;
+                  content: "";
+                  height: 12px;
+                  width: 50px;
+                  margin-top: -1px;
+                  margin-left: -2px;
+                  background-color: rgb(160, 160, 160);
+                  box-shadow: inset 0px 0px 5px rgb(19, 19, 19);
+                  }
+          }
+          &:hover{
+              transform: none;
+              cursor: pointer;
+              & .lightsaber1, .lightsaber2{
+              width: 300px;
+              transition: ease-in 0.4s;
+              }
+              & .lightsaber1{
+                  box-shadow: 40px 0px 20px  red;
+                  transform: rotate(25deg) translateY(92px);
+                  
+              }
+              & .lightsaber2{
+                  box-shadow: 40px 0px 20px blue;
+                  transform: rotate(-25deg) translateY(-92px);
+              }
+
+          }
       }
-    }
-  }
+ }
 </style>
